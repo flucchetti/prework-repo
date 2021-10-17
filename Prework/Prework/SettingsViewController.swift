d37484c7ebbf0345aa61782d56532150e75c1b4e
Francesca <francesca@Francescas-MacBook-Pro.local>
2021-10-16 22:07:31 -0400

    final commit

--- Prework/Prework/SettingsViewController.swift
+++ Prework/Prework/SettingsViewController.swift
@@ -9,21 +9,37 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var defaultTip1Field: UITextField!
+    @IBOutlet weak var defaultTip2Field: UITextField!
+    @IBOutlet weak var defaultTip3Field: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        let defaults = UserDefaults.standard
+        
+        // set defaults
+        let tip1 = Double(defaultTip1Field.text!) ?? 15.0
+        let tip2 = Double(defaultTip2Field.text!) ?? 18.0
+        let tip3 = Double(defaultTip3Field.text!) ?? 20.0
+        
+        defaults.set(tip1, forKey: "defaultTip1")
+        defaults.set(tip2, forKey: "defaultTip2")
+        defaults.set(tip3, forKey: "defaultTip3")
+        
+        defaults.synchronize()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = UserDefaults.standard
+        
+        defaultTip1Field.text = String(defaults.double(forKey: "defaultTip1"))
+        defaultTip2Field.text = String(defaults.double(forKey: "defaultTip2"))
+        defaultTip3Field.text = String(defaults.double(forKey: "defaultTip3"))
     }
-    */
-
 }
--- Prework/Prework/ViewController.swift
+++ Prework/Prework/ViewController.swift
@@ -15,66 +15,77 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var splitAmountLabel: UILabel!
     @IBOutlet weak var partyTextField: UITextField!
-    @IBOutlet weak var sliderLabel: UILabel!
-    
-//    let locationManager = CLLocationManager()
-//    func getSymbol(forCurrencyCode code: String) -> String? {
-//       let locale = NSLocale(localeIdentifier: code)
-//       return locale.displayNameForKey(NSLocaleCurrencySymbol, value: code)
-//    }
+    @IBOutlet weak var partyStepper: UIStepper!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Tip Calculator"
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = UserDefaults.standard
+            
+        // get tip defaults
+        let tip1 = defaults.double(forKey: "defaultTip1")
+        let tip2 = defaults.double(forKey: "defaultTip2")
+        let tip3 = defaults.double(forKey: "defaultTip3")
+            
+        // set tip control
+        let tip1String = String(Int(tip1)) + "%"
+        let tip2String = String(Int(tip2)) + "%"
+        let tip3String = String(Int(tip3)) + "%"
+            
+        tipControl.setTitle(tip1String, forSegmentAt: 0)
+        tipControl.setTitle(tip2String, forSegmentAt: 1)
+        tipControl.setTitle(tip3String, forSegmentAt: 2)
+    }
+    
     func calc_helper(){
-        // Get bill amt from text field
+        // get bill amt and party size from text field
         let bill = Double(billAmountTextField.text!) ?? 0
         let partySize = Int(partyTextField.text!) ?? 1
         
-        // Get total tip: tip * tipPercentage
-        let tipPercentages = [0.15, 0.18, 0.2]
+        // get tip defaults
+        let defaults = UserDefaults.standard
+        let tip1 = defaults.double(forKey: "defaultTip1")
+        let tip2 = defaults.double(forKey: "defaultTip2")
+        let tip3 = defaults.double(forKey: "defaultTip3")
+        
+        // get total tip: tip * tipPercentage
+        let tipPercentages = [tip1/100, tip2/100, tip3/100]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         let splitAmt = total / Double(partySize)
         
         // Update tip amt label
         tipAmountLabel.text = String(format: "$%.2f", tip)
-        // Update total amt
+        // Update total amt label
         totalLabel.text = String(format: "$%.2f", total)
-        // Update per person amt
+        // Update per person amt label
         splitAmountLabel.text = String(format: "$%.2f", splitAmt)
     }
     
     @IBAction func calculateTip(_ sender: Any) {
         calc_helper()
     }
+    
+    @IBAction func changeBillAmount(_ sender: UITextField){
+        calc_helper()
+    }
 
-    @IBAction func changeParty(_ sender: UIStepper) {
-        self.partyTextField.text = Int(sender.value).description
+    @IBAction func changePartyStepper(_ sender: UIStepper) {
+        let stepper_val = Int(partyStepper.value)
+        partyTextField.text = stepper_val.description
         calc_helper()
     }
     
-    @IBAction func changeBillAmount(_ sender: UITextField) {
+    @IBAction func changePartyText(_ sender: UITextField) {
+        let new_party_size = Int(partyTextField.text!) ?? 1
+        partyStepper.value = Double(new_party_size)
         calc_helper()
     }
     
-    @IBAction func changePartySize(_ sender: UITextField) {
-        calc_helper()
-    }
-    
-    @IBAction func setTipSlider(_ sender: UISlider) {
-        sliderLabel.text = String(format: "$%d", Int(sender.value))
-        sender.value = roundf(sender.value)
-        let trackRect = sender.trackRect(forBounds: sender.frame)
-        let thumbRect = sender.thumbRect(forBounds: sender.bounds, trackRect: trackRect, value: sender.value)
-        self.sliderLabel.center = CGPoint(x: thumbRect.midX, y: self.sliderLabel.center.y)
-        let bill = Double(billAmountTextField.text!) ?? 0
-        if( bill != 0 ){
-                calc_helper()
-        }
-        
-    }
 }
 
